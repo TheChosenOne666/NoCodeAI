c4c35d59c31175b83d95fb4c187310c4fc4102f7
TheChosenOne666 <1526655958@qq.com>
2026-01-29 20:11:45 +0800

    对话历史保存数据库bug修复

--- src/main/java/com/xiaolou/xiaolouainocodebackend/service/impl/AppServiceImpl.java
+++ src/main/java/com/xiaolou/xiaolouainocodebackend/service/impl/AppServiceImpl.java
@@ -143,11 +143,13 @@ public class AppServiceImpl extends ServiceImpl<AppMapper, App>
             // 流式响应完成后，添加到对话历史
             String aiResponse = aiResponseBuild.toString();
             if (StrUtil.isNotBlank(aiResponse)){
+                chatHistoryService.addChatMessage(appId, message, ChatHistoryMessageTypeEnum.USER.getValue(), loginUser.getId());
                 chatHistoryService.addChatMessage(appId, aiResponse, ChatHistoryMessageTypeEnum.AI.getValue(), loginUser.getId());
             }
         }).doOnError(error -> {
             // 如果 AI 回复失败也要保存记录
             String errorMessage = "AI回复失败：" + error.getMessage();
+            chatHistoryService.addChatMessage(appId, message, ChatHistoryMessageTypeEnum.USER.getValue(), loginUser.getId());
             chatHistoryService.addChatMessage(appId, errorMessage, ChatHistoryMessageTypeEnum.AI.getValue(), loginUser.getId());
         });
 
