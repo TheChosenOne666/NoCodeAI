95e81ab3a02aba47b439dcfd7e7ae3ac9ff04508
TheChosenOne666 <1526655958@qq.com>
2026-02-01 22:01:28 +0800

    selenium截图实现应用封面图

--- src/main/java/com/xiaolou/xiaolouainocodebackend/service/impl/AppServiceImpl.java
+++ src/main/java/com/xiaolou/xiaolouainocodebackend/service/impl/AppServiceImpl.java
@@ -25,6 +25,7 @@ import com.xiaolou.xiaolouainocodebackend.model.vo.UserVO;
 import com.xiaolou.xiaolouainocodebackend.service.AppService;
 import com.xiaolou.xiaolouainocodebackend.mapper.AppMapper;
 import com.xiaolou.xiaolouainocodebackend.service.ChatHistoryService;
+import com.xiaolou.xiaolouainocodebackend.service.ScreenshotService;
 import com.xiaolou.xiaolouainocodebackend.service.UserService;
 import com.xiaolou.xiaolouainocodebackend.utils.SqlUtils;
 import jakarta.annotation.Resource;
@@ -62,6 +63,9 @@ public class AppServiceImpl extends ServiceImpl<AppMapper, App>
 
     @Resource
     private VueProjectBuilder vueProjectBuilder;
+
+    @Resource
+    private ScreenshotService screenshotService;
 
     @Override
     public AppVO getAppVO(App app) {
@@ -206,8 +210,32 @@ public class AppServiceImpl extends ServiceImpl<AppMapper, App>
         boolean updateResult = this.updateById(updateApp);
         ThrowUtils.throwIf(!updateResult, ErrorCode.OPERATION_ERROR, "更新应用部署信息失败");
         // 10. 返回可访问的 URL
-        return String.format("%s/%s/", AppConstant.CODE_DEPLOY_HOST, deployKey);
+        String appDeployUrl = String.format("%s/%s/", AppConstant.CODE_DEPLOY_HOST, deployKey);
+        generateAppScreenshotAsync(appId, appDeployUrl);
+        return appDeployUrl;
     }
+
+    /**
+     * 异步生成应用截图并更新封面
+     *
+     * @param appId  应用ID
+     * @param appUrl 应用访问URL
+     */
+    @Override
+    public void generateAppScreenshotAsync(Long appId, String appUrl) {
+        // 使用虚拟线程异步执行
+        Thread.startVirtualThread(() -> {
+            // 调用截图服务生成截图并上传
+            String screenshotUrl = screenshotService.generateAndUploadScreenshot(appUrl);
+            // 更新应用封面字段
+            App updateApp = new App();
+            updateApp.setId(appId);
+            updateApp.setCover(screenshotUrl);
+            boolean updated = this.updateById(updateApp);
+            ThrowUtils.throwIf(!updated, ErrorCode.OPERATION_ERROR, "更新应用封面字段失败");
+        });
+    }
+
 
     /**
      * 删除应用时关联删除对话历史
