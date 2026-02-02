b72f8a8e55265a172f4b06f71978392dcfa395dd
TheChosenOne666 <1526655958@qq.com>
2026-02-02 20:19:10 +0800

    增量修改 1、用户再次编辑需要修改指定位置元素时，只会输出指定的修改信息 2、优化工具调用输出效果更具体

--- src/main/java/com/xiaolou/xiaolouainocodebackend/ai/AiCodeGeneratorServiceFactory.java
+++ src/main/java/com/xiaolou/xiaolouainocodebackend/ai/AiCodeGeneratorServiceFactory.java
@@ -2,7 +2,7 @@ package com.xiaolou.xiaolouainocodebackend.ai;
 
 import com.github.benmanes.caffeine.cache.Cache;
 import com.github.benmanes.caffeine.cache.Caffeine;
-import com.xiaolou.xiaolouainocodebackend.ai.tools.FileWriteTool;
+import com.xiaolou.xiaolouainocodebackend.ai.tools.*;
 import com.xiaolou.xiaolouainocodebackend.common.ErrorCode;
 import com.xiaolou.xiaolouainocodebackend.exception.BusinessException;
 import com.xiaolou.xiaolouainocodebackend.model.enums.CodeGenTypeEnum;
@@ -41,6 +41,9 @@ public class AiCodeGeneratorServiceFactory {
 
     @Resource
     private ChatHistoryService chatHistoryService;
+
+    @Resource
+    private ToolManager toolManager;
 
     /**
      * AI 服务实例缓存
@@ -96,7 +99,7 @@ public class AiCodeGeneratorServiceFactory {
             case VUE_PROJECT -> AiServices.builder(AiCodeGeneratorService.class)
                     .streamingChatModel(reasoningStreamingChatModel)
                     .chatMemoryProvider(memoryId -> chatMemory)
-                    .tools(new FileWriteTool())
+                    .tools(toolManager.getAllTools())
                     .hallucinatedToolNameStrategy(toolExecutionRequest -> ToolExecutionResultMessage.from(
                             toolExecutionRequest, "Error: there is no tool called " + toolExecutionRequest.name()
                     ))
